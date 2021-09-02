fa121413d594af3bfd84098d08901028ec04e15a
Rafael Janovicci <rjanovicci@hotmail.com>
2021-09-02 19:06:08 -0300

    criaPageGame

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
+import Game from './pages/Game';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
+      <Route path="/game" component={ Game } />
     </Switch>
   );
 }
