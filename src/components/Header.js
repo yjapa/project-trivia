1e8bf6aa056c3337476daf34ee36f525f63c98c7
ricardo-sousa <rwmsousa@gmail.com>
2021-09-03 11:55:44 -0300

    criação da página game e imagem gravatar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,39 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import md5 from 'crypto-js/md5';
+import PropTypes from 'prop-types';
 
 class Header extends Component {
   render() {
+    const { email } = this.props;
+    const emailGravatar = md5(email).toString();
     return (
       <div className="header">
         <div className="headerGamer">
-          <img src="" alt="" className="gravatar" data-testid="header-profile-picture"/>
-          <p>Jogador: <span data-testid="header-player-name">Salcicha</span></p>
+          <img
+            src="`https://www.gravatar.com/avatar/${emailGravatar}`"
+            alt="gravatar gamer"
+            className="gravatar"
+            data-testid="header-profile-picture"
+          />
+          <p>
+            Jogador:
+            <span data-testid="header-player-name"> Salcicha</span>
+          </p>
         </div>
         <div>
-          <p>Pontos: <span data-testid="header-score">Salcipoints</span></p>
+          <p>
+            Pontos:
+            <span data-testid="header-score"> Salcipoints</span>
+          </p>
         </div>
       </div>
     );
   }
 }
+
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+  emailGravatar: PropTypes.string.isRequired,
+};
 
 export default Header;
