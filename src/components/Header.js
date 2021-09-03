18fce7e781cd46e66fdfc2c3050fa173f6e290a9
ricardo-sousa <rwmsousa@gmail.com>
2021-09-03 11:16:52 -0300

    estrutura do header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,14 @@ import React, {Component} from 'react';
 class Header extends Component {
   render() {
     return (
-      <div>
-
+      <div className="header">
+        <div className="headerGamer">
+          <img src="" alt="" className="gravatar" data-testid="header-profile-picture"/>
+          <p>Jogador: <span data-testid="header-player-name">Salcicha</span></p>
+        </div>
+        <div>
+          <p>Pontos: <span data-testid="header-score">Salcipoints</span></p>
+        </div>
       </div>
     );
   }
