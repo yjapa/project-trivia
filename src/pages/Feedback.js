db6b9d2c57a1b44d449788eb2e3e6ae39e518e7d
ricardo-sousa <rwmsousa@gmail.com>
2021-09-09 16:08:49 -0300

    continuar na máquina do Rafael

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -2,16 +2,16 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Feedback extends React.Component {
-  componentDidMount() {
-    const stateLocalstorage = localStorage.getItem('state');
-    const { assertions } = stateLocalstorage.player;
-    // if (assertions > 2) {
-    //   let congrat = 'Podia ser melhor...';
-    //   congrat = 'Mandou bem!';
-    // }
-    let congrat = 'Podia ser melhor...';
-    assertions > 2? congrat = 'Podia ser melhor...' : 
-  }
+  // componentDidMount() {
+  //   const stateLocalstorage = localStorage.getItem('state');
+  //   const { assertions } = stateLocalstorage.player;
+  //   // if (assertions > 2) {
+  //   //   let congrat = 'Podia ser melhor...';
+  //   //   congrat = 'Mandou bem!';
+  //   // }
+  //   let congrat = 'Podia ser melhor...';
+  //   assertions > 2? congrat = 'Podia ser melhor...'
+  // }
 
   render() {
     return (
