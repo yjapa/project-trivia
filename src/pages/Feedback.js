a932f7d35d568adcf5213f5eb67ab100548f9c79
ricardo-sousa <rwmsousa@gmail.com>
2021-09-09 16:06:41 -0300

    iniciando requisito 13

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -2,11 +2,23 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Feedback extends React.Component {
+  componentDidMount() {
+    const stateLocalstorage = localStorage.getItem('state');
+    const { assertions } = stateLocalstorage.player;
+    // if (assertions > 2) {
+    //   let congrat = 'Podia ser melhor...';
+    //   congrat = 'Mandou bem!';
+    // }
+    let congrat = 'Podia ser melhor...';
+    assertions > 2? congrat = 'Podia ser melhor...' : 
+  }
+
   render() {
     return (
       <div>
         <Header />
-        <p data-testid="feedback-text">Feedback page</p>
+
+        <p data-testid="feedback-text">{ congrat }</p>
       </div>
     );
   }
