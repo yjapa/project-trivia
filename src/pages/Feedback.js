261c6f957519a9fc875e2617af97de2c620ce955
Rafael Janovicci <rjanovicci@hotmail.com>
2021-09-09 16:34:41 -0300

    requi 13

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -2,22 +2,13 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Feedback extends React.Component {
-  // componentDidMount() {
-  //   const stateLocalstorage = localStorage.getItem('state');
-  //   const { assertions } = stateLocalstorage.player;
-  //   // if (assertions > 2) {
-  //   //   let congrat = 'Podia ser melhor...';
-  //   //   congrat = 'Mandou bem!';
-  //   // }
-  //   let congrat = 'Podia ser melhor...';
-  //   assertions > 2? congrat = 'Podia ser melhor...'
-  // }
-
   render() {
+    const stateLocalstorage = JSON.parse(localStorage.getItem('state'));
+    const { assertions } = stateLocalstorage.player;
+    const congrat = assertions > 2 ? 'Mandou bem!' : 'Podia ser melhor...';
     return (
       <div>
         <Header />
-
         <p data-testid="feedback-text">{ congrat }</p>
       </div>
     );
